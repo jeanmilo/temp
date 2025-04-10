ee185907e066f3577647979c458bff95918f0506
Claudia (Dia) <claudia.parlee@gmail.com>
2025-04-10 19:14:30 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  distDir: ".next", //this line will tell the build to create a file with this name
+};
+
+export default nextConfig;
